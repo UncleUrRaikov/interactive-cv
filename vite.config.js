4bc6d603f4cb2f89a38a7328fc9bc510189841ab
UncleRaikoV <niseva@outlook.com>
2025-10-06 16:41:23 -0300

    No workable page on https://uncleurraikov.github.io/interactive-cv/ , its suck!!!

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // ЭТО САМОЕ ВАЖНОЕ ИСПРАВЛЕНИЕ:
-  base: "/interactive-cv/", 
   plugins: [react()],
 });
